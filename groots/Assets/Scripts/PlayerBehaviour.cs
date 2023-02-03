66182dcf20217fb8eb5e58bede5680794d8db818
FESH-pl <94991777+FESH-pl@users.noreply.github.com>
2023-02-03 23:07:51 +0100

    Player face coroutine

--- groots/Assets/Scripts/PlayerBehaviour.cs
+++ groots/Assets/Scripts/PlayerBehaviour.cs
@@ -7,21 +7,74 @@ public class PlayerBehaviour : MonoBehaviour
     // For testing
     public string playerName;
 
+    public Sprite neutral;
+    public Sprite happy;
+    public Sprite upset;
+
     private int score = 0;
 
+    private SpriteRenderer spriteRenderer;
+
+    private Coroutine lastCoroutine = null;
+
+    /// <summary>
+    /// Makes sure player game object won't be destroyed on new scene load
+    /// </summary>
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    /// <summary>
+    /// Handles item collision with player
+    /// Sets sprite to happy/upset
+    /// Updates score
+    /// Destroys item
+    /// </summary>
+    /// <param name="item">item that collided with player</param>
     private void OnCollisionEnter2D(Collision2D item)
     {
+        if(item.gameObject.tag == "Good")
+        {
+            // TODO: handle points better
+            UpdateScore(1);
 
+            // Set sprite to happy and set coroutine to return to neutral
+            spriteRenderer.sprite = happy;
+            if (lastCoroutine != null) StopCoroutine(lastCoroutine);
+            lastCoroutine = StartCoroutine(BackToNeutralFace());
+
+        }
+        else if (item.gameObject.tag == "Bad")
+        {
+            // TODO: handle points better
+            UpdateScore(-1);
+
+            // Set sprite to upset and set coroutine to return to neutral
+            spriteRenderer.sprite = upset;
+            if (lastCoroutine != null) StopCoroutine(lastCoroutine);
+            lastCoroutine = StartCoroutine(BackToNeutralFace());
+        }
+
+        // TODO: particle effects?
+
+        Destroy(item.gameObject);
     }
 
     private void UpdateScore(int points)
     {
         score += points;
+    }
+
+    /// <summary>
+    /// Waits 0.5 seconds then returns player sprite to neutral
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator BackToNeutralFace()
+    {
+        yield return new WaitForSeconds(0.5f);
+        spriteRenderer.sprite = neutral;
     }
 
     // For testing
@@ -30,6 +83,11 @@ public class PlayerBehaviour : MonoBehaviour
     {
         UpdateScore(1);
         Debug.Log(playerName + ":  " + score);
+        spriteRenderer.sprite = happy;
+
+
+        if(lastCoroutine != null) StopCoroutine(lastCoroutine);
+        lastCoroutine = StartCoroutine(BackToNeutralFace());
     }
 
 }
