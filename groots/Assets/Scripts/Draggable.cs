6e5480eba61aec2fe7fd74f5fa951d348d6de8ae
NicovPelt <nicobn@live.nl>
2023-02-04 13:58:11 +0100

    Fixed flicking system

--- groots/Assets/Scripts/Draggable.cs
+++ groots/Assets/Scripts/Draggable.cs
@@ -12,9 +12,9 @@ public class Draggable : MonoBehaviour
         _lastPosition = new Vector3[20];
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdatePosition();
     }
 
     public void UpdatePosition()
@@ -28,6 +28,7 @@ public class Draggable : MonoBehaviour
 
     public void giveVelocity(int multiplier)
     {
+        Debug.Log(_lastPosition[0] + " " + _lastPosition[1] + " " + _lastPosition[2] + " " + _lastPosition[3] + " " + _lastPosition[4] + " " + _lastPosition[5] + " " + _lastPosition[6] + " " + _lastPosition[7] + " " + _lastPosition[8] + " " + _lastPosition[9] + " " + _lastPosition[10] + " " + _lastPosition[11] + " " + _lastPosition[12] + " " + _lastPosition[13] + " " + _lastPosition[14] + " " + _lastPosition[15] + " " + _lastPosition[16] + " " + _lastPosition[17] + " " + _lastPosition[18] + " " + _lastPosition[19]);
         Vector2 givenVelocity = transform.position - _lastPosition[_lastPosition.Length - 1];
         gameObject.GetComponent<Rigidbody2D>().velocity = givenVelocity * multiplier;
         //Debug.Log(givenVelocity);
