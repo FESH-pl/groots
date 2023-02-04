59a4758055fd67ec9d65d1e8fe375d6797a8b588
NicovPelt <nicobn@live.nl>
2023-02-04 20:46:13 +0100

    Finished Score screen, hopefuly

--- groots/Assets/Scripts/ScoreScreenBehaviour.cs
+++ groots/Assets/Scripts/ScoreScreenBehaviour.cs
@@ -6,12 +6,16 @@ using TMPro;
 
 public class ScoreScreenBehaviour : MonoBehaviour
 {
-    public GameObject carrotTop;
-    public GameObject carrotMiddle;
-    public GameObject carrotBottom;
+    public GameObject p1carrotTop;
+    public GameObject p1carrotMiddle;
+    public GameObject p1carrotBottom;
+    public GameObject p2carrotTop;
+    public GameObject p2carrotMiddle;
+    public GameObject p2carrotBottom;
     public GameObject player1;
     public GameObject player2;
     public TextMeshProUGUI winnerText;
+    public GameObject fireworks;
 
     private bool startScoring;
     private float maxPullSpeed = 1f;
@@ -19,8 +23,8 @@ public class ScoreScreenBehaviour : MonoBehaviour
     private float pullSpeedIncrease = 0.0015f;
 
     private float amountPulled = 0f;
-    private int segments1 = 0;
-    private int segments2 = 0;
+    //private int segments1 = 0;
+    //private int segments2 = 0;
     private int p1Score = 10;
     private int p2Score = 20;
     private bool p1Done = false;
@@ -31,8 +35,33 @@ public class ScoreScreenBehaviour : MonoBehaviour
     {
         p1Score = PlayerPrefs.GetInt("p1");
         p2Score = PlayerPrefs.GetInt("p2");
-        Instantiate(carrotTop, player1.transform);
-        Instantiate(carrotTop, player2.transform);
+        //Instantiate(p1carrotTop, player1.transform);
+        //Instantiate(p2carrotTop, player2.transform);
+        
+        if(p1Score < 1)
+        {
+            p1Score = 1;
+        }
+        for(int i = 0; i<p1Score; i++)
+        {
+            GameObject o1 = Instantiate(p1carrotMiddle, player1.transform);
+            o1.transform.localPosition = new Vector3(-0.3f, ((float)i*-9.23f)-12.515f, o1.transform.position.z);
+        }
+        GameObject o = Instantiate(p1carrotBottom, player1.transform);
+        o.transform.localPosition = new Vector3(-0.3f, ((float)p1Score * -9.23f) - 12.515f, o.transform.position.z);
+
+        if (p2Score < 1)
+        {
+            p2Score = 1;
+        }
+        for (int i = 0; i < p2Score; i++)
+        {
+            GameObject o2 = Instantiate(p2carrotMiddle, player2.transform);
+            o2.transform.localPosition = new Vector3(0.3f, ((float)i * -9.23f) - 12.515f, o2.transform.position.z);
+        }
+        o = Instantiate(p2carrotBottom, player2.transform);
+        o.transform.localPosition = new Vector3(0.3f, ((float)p2Score * -9.23f) - 12.515f, o.transform.position.z);
+
         StartCoroutine(StartDelay());
     }
 
@@ -42,48 +71,48 @@ public class ScoreScreenBehaviour : MonoBehaviour
 
         if (startScoring)
         {
-            //if (!p1Done)
-            //{
+            if (!p1Done)
+            {
                 player1.transform.position = new Vector3(player1.transform.position.x, player1.transform.position.y + pullSpeed, player1.transform.position.z);
-            //}
-            //if (!p2Done)
-            //{
+            }
+            if (!p2Done)
+            {
                 player2.transform.position = new Vector3(player2.transform.position.x, player2.transform.position.y + pullSpeed, player2.transform.position.z);
-            //}
+            }
 
             amountPulled += pullSpeed;
-            if(segments1 < p1Score && segments1 < (int)amountPulled)
+            /*if(segments1 < p1Score && segments1 < (int)amountPulled)
             {
                 segments1++;
-                GameObject o1 = Instantiate(carrotMiddle, player1.transform);
+                GameObject o1 = Instantiate(p1carrotMiddle, player1.transform);
                 o1.transform.position = new Vector3(o1.transform.position.x, o1.transform.position.y - segments1, o1.transform.position.z);
             } else if (segments1 >= p1Score && !p1Done)
             {
                 segments1++;
-                GameObject o1 = Instantiate(carrotBottom, player1.transform);
+                GameObject o1 = Instantiate(p1carrotBottom, player1.transform);
                 o1.transform.position = new Vector3(o1.transform.position.x, o1.transform.position.y - segments1, o1.transform.position.z);
                 p1Done = true;
             }
             if(segments2 < p2Score && segments2 < (int)amountPulled)
             {
                 segments2++;
-                GameObject o2 = Instantiate(carrotMiddle, player2.transform);
+                GameObject o2 = Instantiate(p2carrotMiddle, player2.transform);
                 o2.transform.position = new Vector3(o2.transform.position.x, o2.transform.position.y - segments2, o2.transform.position.z);
             }
             else if (segments2 >= p2Score && !p2Done)
             {
                 segments2++;
-                GameObject o2 = Instantiate(carrotBottom, player2.transform);
+                GameObject o2 = Instantiate(p2carrotBottom, player2.transform);
                 o2.transform.position = new Vector3(o2.transform.position.x, o2.transform.position.y - segments2, o2.transform.position.z);
                 p2Done = true;
-            }
+            }*/
 
             if (!(pullSpeed >= maxPullSpeed))
             {
                 pullSpeed += pullSpeedIncrease;
             }
 
-            if (p1Done && p2Done && !endState)
+            if (amountPulled > (20 + (p1Score*10)) && amountPulled > (20 + (p2Score * 10)) && !endState)
             {
                 StartCoroutine(AnnounceWinner());
                 endState = true;
@@ -101,6 +130,7 @@ public class ScoreScreenBehaviour : MonoBehaviour
     IEnumerator AnnounceWinner()
     {
         yield return new WaitForSeconds(2);
+        fireworks.SetActive(true);
         if(p1Score > p2Score)
         {
             winnerText.text = "Red Player Wins!";
