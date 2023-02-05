734f363df06079698dd4dac5a0e7078a0942b93e
FESH-pl <94991777+FESH-pl@users.noreply.github.com>
2023-02-05 12:55:55 +0100

    title screen grannies

--- groots/Assets/Scripts/TitlePlayBehaviour.cs
+++ groots/Assets/Scripts/TitlePlayBehaviour.cs
@@ -8,6 +8,9 @@ public class TitlePlayBehaviour : MonoBehaviour
     public GameObject leftGrandma;
     public GameObject rightGrandma;
 
+    public GameObject leftGrandmaName;
+    public GameObject rightGrandmaName;
+
     private bool pressedPlay = false;
     private void OnMouseDown()
     {
@@ -20,7 +23,10 @@ public class TitlePlayBehaviour : MonoBehaviour
         if (pressedPlay)
         {
             leftGrandma.transform.position = new Vector3(leftGrandma.transform.position.x - 20f, leftGrandma.transform.position.y, leftGrandma.transform.position.z);
+            leftGrandmaName.transform.position = new Vector3(leftGrandmaName.transform.position.x - 20f, leftGrandmaName.transform.position.y, leftGrandmaName.transform.position.z);
+
             rightGrandma.transform.position = new Vector3(rightGrandma.transform.position.x + 20f, rightGrandma.transform.position.y, rightGrandma.transform.position.z);
+            rightGrandmaName.transform.position = new Vector3(rightGrandmaName.transform.position.x + 20f, rightGrandmaName.transform.position.y, rightGrandmaName.transform.position.z);
         }
     }
 
